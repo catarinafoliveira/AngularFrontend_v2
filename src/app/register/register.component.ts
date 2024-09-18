8112dca2bcdfd690d0f3157c53ded4d256cc987a
catarinafoliveira <felixcata@gmail.com>
2024-09-18 15:30:24 +0100

    Register form layout

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  username: string = "";
+  idCard: string = "";
+  isEmployee: boolean = true;
+  licence: string = "";
+  password: string = "";
+  passwordStrengthLabel: string = "";
+  password2: string = "";
 
+  register(): void{
+
+  }
+
+  checkPasswordStrength(): void {
+
+  }
+
+  toggleShowPassword(){
+
+  }
+
+  toggleShowPassword2(){
+
+  }
 }
