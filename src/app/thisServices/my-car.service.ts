06b827e18741c36dd364c528c666a80530a7e7ed
catarinafoliveira <felixcata@gmail.com>
2024-09-23 12:15:05 +0100

    MyCars Component

--- src/app/thisServices/my-car.service.ts
+++ src/app/thisServices/my-car.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Car } from '../thisModels/car';
+import { AuthenticatorService } from './authenticator.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MyCarService {
-
-  constructor() { }
+  private apiUrl = "http://localhost:8080/api/cars" ;
+  
+  constructor(
+    private http: HttpClient,
+    private authenticatorService: AuthenticatorService
+  ){}
+  
+  getCars(licence: string): Observable<Car[]>{
+    return this.http.get<Car[]>(this.apiUrl+"/owner/"+licence, { headers: this.authenticatorService.getHeaders() });
+  }
+  
+  createCar(car: Car): Observable<Car> {
+    return this.http.post<Car>(this.apiUrl, car, { headers: this.authenticatorService.getHeaders() });
+  }
 }
